b59463568d863793ad1e84fa594e5ae56ed25687
kim_minseok <kmus1232@gmail.com>
2022-05-02 21:58:15 +0900

    .

---
+++ validation/src/main/java/hello/itemservice/web/validation/ValidationItemApiController.java
@@ -0,0 +1,27 @@
+package hello.itemservice.web.validation;
+
+import hello.itemservice.web.validation.form.ItemSaveForm;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequestMapping("/validation/api/items")
+public class ValidationItemApiController {
+
+    @PostMapping("/add")
+    public Object addItem(@RequestBody @Validated ItemSaveForm form, BindingResult bindingResult) {
+
+        log.info("API 컨트롤러 호출");
+
+        if (bindingResult.hasErrors()) {
+            log.info("errors = {}", bindingResult);
+            return bindingResult.getAllErrors();
+        }
+
+        log.info("로직 성공");
+        return form;
+    }
+}
